b7199e592eea951c7d877cb3bf8afbe69867ce1d
Mateus-Scarin <mateus.scarin@gmail.com>
2022-06-08 21:33:32 -0300

    Inicialização CRUD completo

---
+++ src/main/java/com/api/parkingcontrol/repository/ParkingSpotRepository.java
@@ -0,0 +1,23 @@
+package com.api.parkingcontrol.repository;
+
+import com.api.parkingcontrol.models.ParkingSpotModel;
+import java.util.UUID;
+import org.springframework.boot.autoconfigure.data.web.SpringDataWebProperties;
+import org.springframework.data.domain.Page;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+/**
+ *
+ * @author Mateus
+ */
+@Repository
+public interface ParkingSpotRepository extends JpaRepository<ParkingSpotModel, UUID> {
+
+    boolean existsByLicensePlateCar(String licensePlateCar);
+
+    boolean existsByParkingSpotNumber(String parkingSpotNumber);
+
+    boolean existsByApartmentAndBlock(String apartment, String block);
+
+}
